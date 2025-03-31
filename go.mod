610b974943544b02fbdff1a6786135e0a17439a9
RuizLenato <ruizlenato@proton.me>
2025-03-30 21:58:20 -0300

    chore: remove test dependencies and simplify test assertions
    - Remove stretchr/testify dependency from go.mod/go.sum
    - Replace assert-based tests with direct error checking in player_test.go
    - Clean up remaining test-related dependencies in go.mod
    - Simplify test assertions using standard library testing methods

--- go.mod
+++ go.mod
@@ -6,16 +6,12 @@ require (
 	github.com/dop251/goja v0.0.0-20250125213203-5ef83b82af17
 	github.com/mengzhuo/cookiestxt v1.0.4
 	github.com/patrickmn/go-cache v2.1.0+incompatible
-	github.com/stretchr/testify v1.10.0
 	golang.org/x/exp v0.0.0-20250128182459-e0ece0dbea4c
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dlclark/regexp2 v1.11.4 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	github.com/google/pprof v0.0.0-20230207041349-798e818bf904 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/text v0.3.8 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- player_test.go
+++ player_test.go
@@ -6,33 +6,48 @@ import (
 	"testing"
 
 	"github.com/dop251/goja/parser"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestPlayer(t *testing.T) {
 	p, err := NewPlayer()
 	if err != nil {
-		t.Error(err)
+		t.Errorf("failed to create player: %v", err)
 	}
 
-	// Test if we extracted all information.
-	assert.NotEmpty(t, p.sig_sc)
-	assert.NotZero(t, p.sig_timestamp)
-	assert.NotEmpty(t, p.nsig_name)
-	assert.NotEmpty(t, p.nsig_sc)
-	assert.NotEmpty(t, p.nsig_check)
-	assert.NotEmpty(t, p.visitorData)
+	if p.sig_sc == "" {
+		t.Errorf("p.sig_sc is empty")
+	}
+	if p.sig_timestamp == 0 {
+		t.Errorf("p.sig_timestamp is zero")
+	}
+	if p.nsig_name == "" {
+		t.Errorf("p.nsig_name is empty")
+	}
+	if p.nsig_sc == "" {
+		t.Errorf("p.nsig_sc is empty")
+	}
+	if p.nsig_check == "" {
+		t.Errorf("p.nsig_check is empty")
+	}
+	if p.visitorData == "" {
+		t.Errorf("p.visitorData is empty")
+	}
 
-	// Test if we can decode the visitor data.
 	visitorData, err := url.QueryUnescape(p.visitorData)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("failed to unescape visitorData: %v", err)
+	}
 	_, err = base64.URLEncoding.DecodeString(visitorData)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("failed to decode visitorData: %v", err)
+	}
 
-	// Test if we can parse the extracted javascript.
 	_, err = parser.ParseFile(nil, "", p.nsig_sc, 0)
-	assert.NoError(t, err)
-
+	if err != nil {
+		t.Errorf("failed to parse p.nsig_sc: %v", err)
+	}
 	_, err = parser.ParseFile(nil, "", p.sig_sc, 0)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("failed to parse p.sig_sc: %v", err)
+	}
 }
